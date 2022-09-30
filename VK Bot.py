4d0d1f833849a9d396bf6090790430c6dae8e540
TPODAvia <64729573+TPODAvia@users.noreply.github.com>
2022-09-30 12:08:10 +0300

    VK Bot redesign 30/09/2022

--- VK Bot.py
+++ VK Bot.py
@@ -12,58 +12,50 @@ from random import randint
 session = vk_api.VkApi(token="vk1.a.O9TFQM6vudNuJx71PbfsspzlUV2OUPLdXcBVmpImkj4tYbXAVyv8c_aceNAfMJ4UlX_x29g5wUJ99utM76-H0p23nuP0SOl1pno5DGisVs_qkuhpr0P3JH2dE3nMNuyWH23SWOe2IvrCGav1LAlyaeXA5j9EI681Qm1sYsIYBOf7x2OYfn-SaP8BGX6JBWPs")
 vk = session.get_api()
 
-def get_user_status(user_id):
-    status = session.method("status.get",{"user_id": user_id})
-    print(status["text"])
-
-def send_user_message(user_id, text):
-    session.method("messages.send",{"random_id": 0 ,"peer_id": user_id, "message": text})
-    
-#Done____________________________________________________________________________
-#get_user_status()
-send_user_message(487724496, "Hello World!")
-
-
-
-#Experiment____________________________________________________________________________
-from vk_api.longpoll import VkLongPoll, VkEventType
-
-def get_user_message(user_id):
-    message = session.method("messages.get",{"user_id": user_id, "filters": 8})
-    print(message)
-
-chat_id=9
-user_id = 290618168
-
-#f = open('new.txt', "x", encoding="utf-8")
-f = open('new.txt', "+w", encoding="utf-8")
-
-lastMessage = vk.messages.getHistory(count =1, peer_id=2000000000+chat_id, user_id=user_id)
-pages=int(lastMessage['items'][0]['id']/200+1)
-print("Pages (page -200 messages): " + str(pages))
-
-history = vk.messages.getHistory(count=2, peer_id=487724496, user_id=user_id)
-history = history['items']
-for j in reversed(history):
-    f.write(str(j)+"\n")
-
-print(str(pages)+" out of " + str(pages))
-f.close()
+#GET UNREAD MODULE ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+unread = session.method("messages.getConversations",{"filter": "unread"})
+ItemsUnread = unread['items']
+for j in reversed(ItemsUnread):
+    str(j)
 print("Done Reading")
 
+FromId = 112856336
 
-#GET TEXT MODULE ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-with open('D:/Coding API/new.txt', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-count = 0
+if (ItemsUnread == []):
+    print("No new messages")
+else:
+    print("From ID: " + str(unread['items'][0]['last_message']['from_id']))
+    print("Date Is: " + str(unread['items'][0]['last_message']['date']))
+    print("Text Is: " + str(unread['items'][0]['last_message']['text']))
 
-# Strips the newline character
-proverbs = np.array([])
-for line in lines:
-    count += 1
-    #print("Line{}: {}".format(count, line.strip()))
-    proverbs = np.append(proverbs, line.strip())
+    
+    if (unread['items'][0]['last_message']['from_id'] == FromId):
+        
+        #GET TEXT MODULE +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        with open('D:/Coding API/new.txt', 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+        
+        count = 0
+        # Strips the newline character
+        proverbs = np.array([])
+        for line in lines:
+            count += 1
+            #print("Line{}: {}".format(count, line.strip()))
+            proverbs = np.append(proverbs, line.strip())
+        use_proverbs = proverbs[0]
 
-use_proverbs = proverbs[0]
+        def send_user_message(user_id, text):
+            session.method("messages.send",{"random_id": 0 ,"peer_id": user_id, "message": text})
 
-print(proverbs[0]==str(j))
+        if (proverbs[0]==str(j)):
+            send_user_message(290618168, "Не надо писать одной тоже пожалуйста")
+        else:
+            send_user_message(290618168, "День добрый отвечает бот. Я добавил вас в список автоответчика. Хозяйн больше не хочет отвечать ни на какие сообщения")
+
+        f = open('new.txt', "+w", encoding="utf-8")
+        for j in reversed(ItemsUnread):
+            f.write(str(j)+"\n")
+
+        f.close()
+        print("Done Writing")
+        
